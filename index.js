49d673df44400cce6f5041742b92aa05da380a2d
new-psam <marcelino.patricio@hotmail.com>
2025-08-03 10:40:53 -0300

    aula 2 async/await assincrono

--- index.js
+++ index.js
@@ -1,21 +1,55 @@
 import fs from 'fs';
 import chalk from 'chalk';
 
+
 function trataErro(erro){
     console.log(erro);
-    throw new Error(chalk.red(erro.code, 'não há arquivo no diretório'));
+    //throw new Error(chalk.red(erro.code, 'não há arquivo no diretório'));
+    console.error(chalk.red(erro.code, 'não há arquivo no diretório'));
 }
 
-function pegarArquivo(caminhoDoArquivo){
-    const encoding = 'utf-8';
-    fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
-        if (erro){
-            trataErro(erro);
-        }
-        console.log(chalk.green(texto));
-    });
+//*** async / await */
+async function pegaArquivo(caminhoDoArquivo){
+    try{
+        const encoding = 'utf-8';
+        const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
+        console.log(chalk.blue(texto));
+    }catch (erro)  {
+        trataErro(erro);
+    } finally {
+        console.log(chalk.yellow('A operação foi concluída!!'));
+    }
+    
 }
 
-pegarArquivo('./arquivos/');
 
-//pegarArquivo('./resumo.txt');
+//*** promises com then */
+// function pegaArquivo(caminhoDoArquivo) {
+//     const encoding = 'utf-8';
+//     fs.promises
+//         .readFile(caminhoDoArquivo, encoding)
+//         .then((texto) => console.log(chalk.green(texto)))
+//         .catch(trataErro);
+// }
+
+//****método sincrono */
+// function pegaArquivo(caminhoDoArquivo){
+//     const encoding = 'utf-8';
+//     fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
+//         if (erro){
+//             trataErro(erro);
+//         }
+//         console.log(chalk.green(texto));
+//     });
+// }
+
+
+
+// pegaArquivo('./resumos/aula1.txt');
+// console.log();
+// pegaArquivo('./resumos/aula2.txt');
+
+pegaArquivo('./arquivos/texto.md');
+console.log();
+console.log();
+pegaArquivo('./arquivos/');
